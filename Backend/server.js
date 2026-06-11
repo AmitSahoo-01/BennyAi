2d80bd7f932a4602a4ad3f058f3511165146536b
Amit Sahoo <amitkusahoo1212@gmail.com>
2026-06-11 08:45:45 +0530

    readding

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,20 @@
 import "dotenv/config";
 import app from "./src/app.js";
+import http from "http";
 import connectToDB from "./src/config/database.js";
+import { initSocket } from "./src/sockets/server.socket.js";
 
 // import {testAi} from "./src/services/ai.service.js";
 
 const PORT = process.env.PORT || 3000;
 
+
+const httpSeerver = http.createServer(app);
+
+initSocket(httpSeerver);
+
 connectToDB().then(()=>{
-    app.listen(PORT,()=>{
+    httpSeerver.listen(PORT,()=>{
         console.log("Server is running on port 3000");
     });
 }).catch((err)=>{
